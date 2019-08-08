0b3eb7ef901706d6a9f0d26ac774f0f8a67367ab
beyzaksy3 <aksoybeyza690mail.com>
2019-08-08 15:11:02 +0300

    ...

---
+++ Form1.Designer.cs
@@ -0,0 +1,359 @@
+﻿namespace WindowsFormsApp10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.HESAPLA = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxAA = new System.Windows.Forms.TextBox();
+            this.textBoxBTC = new System.Windows.Forms.TextBox();
+            this.textBoxSS = new System.Windows.Forms.TextBox();
+            this.BSS = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label15 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BTC";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ADET";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(0, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ALIŞ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(289, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "btc";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(0, 220);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "SATIŞ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(289, 220);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "btc";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.label7.Location = new System.Drawing.Point(25, 274);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "SONUÇ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 309);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(41, 20);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "FEE";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 354);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(83, 20);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "KAR(BTC)";
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(76, 6);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(205, 26);
+            this.textBoxB.TabIndex = 10;
+            this.textBoxB.TextChanged += new System.EventHandler(this.textBoxA_TextChanged);
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(76, 63);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(205, 26);
+            this.textBoxA.TabIndex = 11;
+            // 
+            // HESAPLA
+            // 
+            this.HESAPLA.BackColor = System.Drawing.Color.Blue;
+            this.HESAPLA.ForeColor = System.Drawing.Color.White;
+            this.HESAPLA.Location = new System.Drawing.Point(166, 444);
+            this.HESAPLA.Name = "HESAPLA";
+            this.HESAPLA.Size = new System.Drawing.Size(162, 44);
+            this.HESAPLA.TabIndex = 13;
+            this.HESAPLA.Text = "HESAPLA";
+            this.HESAPLA.UseVisualStyleBackColor = false;
+            this.HESAPLA.Click += new System.EventHandler(this.HESAPLA_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Blue;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(4, 444);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 44);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "TEMİZLE";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxAA
+            // 
+            this.textBoxAA.Location = new System.Drawing.Point(76, 129);
+            this.textBoxAA.Name = "textBoxAA";
+            this.textBoxAA.Size = new System.Drawing.Size(205, 26);
+            this.textBoxAA.TabIndex = 15;
+            // 
+            // textBoxBTC
+            // 
+            this.textBoxBTC.Location = new System.Drawing.Point(346, 129);
+            this.textBoxBTC.Name = "textBoxBTC";
+            this.textBoxBTC.Size = new System.Drawing.Size(194, 26);
+            this.textBoxBTC.TabIndex = 16;
+            // 
+            // textBoxSS
+            // 
+            this.textBoxSS.Location = new System.Drawing.Point(76, 214);
+            this.textBoxSS.Name = "textBoxSS";
+            this.textBoxSS.Size = new System.Drawing.Size(194, 26);
+            this.textBoxSS.TabIndex = 17;
+            // 
+            // BSS
+            // 
+            this.BSS.Location = new System.Drawing.Point(346, 217);
+            this.BSS.Name = "BSS";
+            this.BSS.Size = new System.Drawing.Size(194, 26);
+            this.BSS.TabIndex = 18;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(12, 390);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(63, 20);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "KAR(S)";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(137, 360);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(104, 20);
+            this.label12.TabIndex = 21;
+            this.label12.Text = "-------------------";
+            this.label12.Click += new System.EventHandler(this.label12_Click);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(141, 396);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(84, 20);
+            this.label13.TabIndex = 22;
+            this.label13.Text = "---------------";
+            this.label13.Click += new System.EventHandler(this.label13_Click);
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(97, 309);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(104, 20);
+            this.label14.TabIndex = 23;
+            this.label14.Text = "-------------------";
+            this.label14.Click += new System.EventHandler(this.label14_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(546, 5);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(506, 525);
+            this.dataGridView1.TabIndex = 25;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Blue;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(334, 444);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(162, 44);
+            this.button2.TabIndex = 26;
+            this.button2.Text = "EKLE";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button3.Location = new System.Drawing.Point(346, 318);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(178, 46);
+            this.button3.TabIndex = 27;
+            this.button3.Text = "SATIR SİL";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(240, 396);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(79, 20);
+            this.label15.TabIndex = 24;
+            this.label15.Text = "--------------";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1054, 590);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.BSS);
+            this.Controls.Add(this.textBoxSS);
+            this.Controls.Add(this.textBoxBTC);
+            this.Controls.Add(this.textBoxAA);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.HESAPLA);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "HESAPLAMA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.Button HESAPLA;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxAA;
+        private System.Windows.Forms.TextBox textBoxBTC;
+        private System.Windows.Forms.TextBox textBoxSS;
+        private System.Windows.Forms.TextBox BSS;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label15;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp10
+{
+    public partial class Form1 : Form
+    {
+        DataTable tablo = new DataTable();
+        public static double FEE = 0.002;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void HESAPLA_Click(object sender, EventArgs e)
+        {
+            double FEE = 0.002;
+
+            double alis = 0;
+            double satis = 0;
+            if (!String.IsNullOrEmpty(textBoxB.Text))
+            {
+                alis = ((Convert.ToDouble(textBoxAA.Text) / 100000000)) * Convert.ToDouble(textBoxB.Text) * Convert.ToDouble(textBoxA.Text);
+                satis = ((Convert.ToDouble(textBoxSS.Text) / 100000000)) * Convert.ToDouble(textBoxB.Text) * Convert.ToDouble(textBoxA.Text);
+                var alisfee = alis * FEE;
+                var satisfee = satis * FEE;
+                double fee = 0;
+                fee = (alisfee + satisfee);
+                double karbtc = 0;
+                karbtc = ((Convert.ToDouble(textBoxSS.Text)) * (Convert.ToDouble(textBoxA.Text)) - (Convert.ToDouble(textBoxSS.Text)) * (Convert.ToDouble(textBoxA.Text)) * FEE - (Convert.ToDouble(textBoxAA.Text)) * (Convert.ToDouble(textBoxA.Text)) * FEE) / 100000000;
+                double karusd = 0;
+                karusd = karbtc * (Convert.ToDouble(textBoxB.Text));
+                label14.Text = fee.ToString();
+                label12.Text = karbtc.ToString();
+                label13.Text = karusd.ToString();
+                tablo.Rows.Add(textBoxA.Text, textBoxB.Text, textBoxAA.Text, textBoxSS.Text, label14.Text, label13.Text, label12.Text);
+                dataGridView1.DataSource = tablo;
+            }
+
+            else if (textBoxBTC.Text != null)
+            {
+
+                alis = ((Convert.ToDouble(textBoxAA.Text) / 100000000)) * Convert.ToDouble(textBoxBTC.Text) * Convert.ToDouble(textBoxA.Text);
+                satis = ((Convert.ToDouble(textBoxSS.Text) / 100000000)) * Convert.ToDouble(textBoxSS.Text)* Convert.ToDouble(textBoxA.Text);
+                var alisfee = alis * FEE;
+
+                var satisfee = satis * FEE;
+                double fee = 0;
+
+                fee = (alisfee + satisfee);
+                double karbtc = 0;
+                karbtc = (Convert.ToDouble(textBoxSS.Text) * Convert.ToDouble(textBoxA.Text) - Convert.ToDouble(textBoxSS.Text) * Convert.ToDouble(textBoxA.Text) * FEE - Convert.ToDouble(textBoxAA.Text) * Convert.ToDouble(textBoxA.Text) - Convert.ToDouble(textBoxAA.Text) * Convert.ToDouble(textBoxA.Text) * FEE) / 100000000;
+                double karusd = 0;
+                karusd = ((Convert.ToDouble(textBoxBTC.Text)) * (Convert.ToDouble(BSS.Text)));
+                double karusd2;
+                karusd2 = (((Convert.ToDouble(textBoxSS.Text)) * (Convert.ToDouble(textBoxA.Text)) * (Convert.ToDouble(BSS.Text))) - ((Convert.ToDouble(textBoxAA.Text)) * (Convert.ToDouble(textBoxA.Text)) * (Convert.ToDouble(textBoxBTC.Text)) * 0.098)) / 100000000;
+                label14.Text = fee.ToString();
+                label12.Text = karbtc.ToString();
+                label13.Text = karusd.ToString();
+                label15.Text = karusd2.ToString();
+
+                tablo.Rows.Add(textBoxA.Text, textBoxB.Text, textBoxAA.Text, textBoxSS.Text, label14.Text, label13.Text, label12.Text);
+                dataGridView1.DataSource = tablo;
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+
+        private void textBoxA_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label14.Text = "";
+            label12.Text = "";
+            label13.Text = "";
+            label15.Text = "";
+            textBoxA.Text = "";
+            textBoxAA.Text = "";
+            textBoxB.Text = "";
+            textBoxBTC.Text = "";
+            textBoxSS.Text = "";
+            textBoxSS.Text = "";
+            
+
+
+
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tablo.Columns.Add("BTC", typeof(double));
+            tablo.Columns.Add("ADET", typeof(double));
+            tablo.Columns.Add("ALIŞ", typeof(double));
+            tablo.Columns.Add("SATIŞ", typeof(double));
+            tablo.Columns.Add("FEE", typeof(double));
+            tablo.Columns.Add("KAR(BTC)", typeof(double));
+            tablo.Columns.Add("KAR(S)", typeof(double));
+            
+
+
+            dataGridView1.DataSource = tablo;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                dataGridView1.Rows.RemoveAt(dataGridView1.SelectedRows[0].Index);
+            }
+            else
+            {
+                MessageBox.Show("lütfen silinecek satırı seçin");
+            }
+
+        }
+
+        private void label15_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
